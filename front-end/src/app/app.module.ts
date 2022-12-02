b409b87213063c906bb68eeabc0e0c722396e803
bruno <bruno.lorenzo.arroyo@gmail.com>
2022-12-02 11:43:02 +0000

    front

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SingInComponent,
+    SingUpComponent,
+    HomepageComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule
