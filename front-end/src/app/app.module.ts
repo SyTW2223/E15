89a299b16931b4ca96a7bf250ba8ca87c4b5f703
bruno <bruno.lorenzo.arroyo@gmail.com>
2022-12-02 19:57:44 +0000

    distribución del front-end, materialize
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { SingInComponent } from './sing-in/sing-in.component';
-import { SingUpComponent } from './sing-up/sing-up.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ProfileComponent } from './profile/profile.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SingInComponent } from './components/sing-in/sing-in.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
     FooterComponent,
     HeaderComponent,
     SingInComponent,
     SingUpComponent,
-    HomepageComponent,
-    ProfileComponent,
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule
