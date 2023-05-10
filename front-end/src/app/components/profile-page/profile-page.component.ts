e1e929e236787ac7e8f65e828e65ee823e2f6319
alu0101123677 <alu0101123677@ull.edu.es>
2023-05-10 20:52:32 +0100

    mc
    Co-authored-by: alu0101101507 <alu0101101507@users.noreply.github.com>
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>

--- front-end/src/app/components/profile-page/profile-page.component.ts
+++ front-end/src/app/components/profile-page/profile-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css']
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit {
+  token: any;
+  user: any;
+  
+  constructor(public authService: AuthenticationService) { }
+  ngOnInit() {
+    this.token = this.authService.getToken();
+    this.token = jwt_decode(this.token);
+    this.user = this.token.user;
+    console.log(this.user);
+  }
 
 }
