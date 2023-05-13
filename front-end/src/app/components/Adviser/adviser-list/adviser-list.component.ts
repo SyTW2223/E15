8b2d6c43f2cb96085bf512431e7db9e394a94ce1
alu0101123677 <alu0101123677@ull.edu.es>
2023-05-13 20:45:20 +0100

    feat[frontend]: add exercise
    Co-authored-by: alu0101101507 <alu0101101507@users.noreply.github.com>
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>

---
+++ front-end/src/app/components/Adviser/adviser-list/adviser-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adviser-list',
+  templateUrl: './adviser-list.component.html',
+  styleUrls: ['./adviser-list.component.css']
+})
+export class AdviserListComponent {
+
+}
--- front-end/src/app/components/Exercise/exercise-page/exercise-page.component.ts
+++ front-end/src/app/components/Exercise/exercise-page/exercise-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { GetExercisesService } from 'src/app/services/get-exercises.service';
 
 @Component({
   selector: 'app-exercise-page',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./exercise-page.component.css']
 })
 export class ExercisePageComponent {
+  displayedColumns: string[] = ['name', 'author', 'category', 'short-description', 'equipment'];
+  dataSource: any;
+  categories: any = [];
+  exercises: any = [];
+  equipment: string[] = [];
+  categoryTerm: string = '';
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  constructor(private getExercises: GetExercisesService) {}
 
+  ngOnInit() {
+    this.getExercises.getExercises()
+    .subscribe(
+      res =>  {
+        this.exercises = res
+        this.dataSource = new MatTableDataSource(this.exercises);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        for (let exercise of res) {
+          //TODO: cuando esté el enum iterar sobre el enum, y comprobar que la categoria ya está insertada
+          this.categories.push(exercise.category);
+        }
+      }
+    )
+  }
+
+  applyTextFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  applyCategoryFilter() {
+    this.dataSource.filter = this.categoryTerm.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
