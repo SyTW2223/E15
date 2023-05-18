2877b28f15c10bfe43408673f959e23930aacc18
alu0101123677 <alu0101123677@ull.edu.es>
2023-05-18 20:14:51 +0100

    feat[frontend]: add adviser

--- front-end/src/app/components/Adviser/adviser-list/adviser-list.component.ts
+++ front-end/src/app/components/Adviser/adviser-list/adviser-list.component.ts
@@ -3,32 +3,45 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { UsersService } from 'src/app/services/users.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 //import { User } from './back-end/Schema/userSchema';
 
 @Component({
   selector: 'app-adviser-list',
   templateUrl: './adviser-list.component.html',
-  styleUrls: ['./adviser-list.component.css']
+  styleUrls: ['./adviser-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
+
 export class AdviserListComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'email', 'role', 'phone_number'];
+  displayedColumns: string[] = ['first_name', 'email', 'role', 'phone_number'];
   dataSource: any;
   categories: any = [];
   users: any = [];
   emailTerm: string = '';
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  columnsToDisplayWithExpand = [...this.displayedColumns, 'expand'];
+  expandedElement: any | null;
   
   constructor(private getUsers: UsersService) {}
 
   ngOnInit() {
-    this.getUsers.getUser()
+    this.getUsers.getUsers()
     .subscribe(
       res =>  {
         this.users = res.filter((user: any) => user.role === 'Entrenador')
         this.dataSource = new MatTableDataSource(this.users);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
+
+        
         for (let user of res) {
           //TODO: cuando esté el enum iterar sobre el enum, y comprobar que la categoria ya está insertada
           this.categories.push(user.first_name);
@@ -53,4 +66,11 @@ export class AdviserListComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
+
+  columnNames: { [key: string]: string } = {
+    first_name: 'Nombre ',
+    email: 'Correo Electrónico',
+    role: 'Rol',
+    phone_number: 'Número de Teléfono'
+  };
 }
