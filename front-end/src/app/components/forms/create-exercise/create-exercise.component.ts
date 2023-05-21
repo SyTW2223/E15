4667b75053e037b69d2982dcd03429f5ff8659b3
alu0101123677 <alu0101123677@ull.edu.es>
2023-05-21 13:16:44 +0100

    feat[frontend]: create exercise
    Co-authored-by: alu0101101507 <alu0101101507@users.noreply.github.com>
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>

--- front-end/src/app/components/forms/create-exercise/create-exercise.component.ts
+++ front-end/src/app/components/forms/create-exercise/create-exercise.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ExercisesService } from 'src/app/services/exercises.service';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
+
 
 @Component({
   selector: 'app-create-exercise',
   templateUrl: './create-exercise.component.html',
   styleUrls: ['./create-exercise.component.css']
 })
-export class CreateExerciseComponent {
+export class CreateExerciseComponent implements OnInit {
+  token: any;
+  exercise: any = {};
+
+  constructor(private exerciseService: ExercisesService,
+    public authService: AuthenticationService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.token = this.authService.getToken();
+    this.token = jwt_decode(this.token);
+  }
+
+  createExercise() {
+    this.exercise.author = this.token.user._id;
+    this.exercise.picture = "Imagen";
+    this.exercise.likes = 0;
+    this.exercise.comments = [];
+    if (this.exercise.equipment_needed == "Si") {
+      this.exercise.equipment_needed = true;
+    } else {
+      this.exercise.equipment_needed = false;
+    }
+    this.exerciseService.postExercise(this.exercise)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.router.navigate(['/profile']);          
+        },
+        err => console.log(err)
+      ) 
+  }
 
 }
