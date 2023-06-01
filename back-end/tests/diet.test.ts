599d5dad7981f9c2286d2ca8a00f94beb55a5fa5
alu0101123677 <alu0101123677@ull.edu.es>
2023-06-01 19:58:44 +0100

    test[backend]: update diets test
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>
    Co-authored-by: alu0101101507 <alu0101101507@users.noreply.github.com>

--- back-end/tests/diet.test.ts
+++ back-end/tests/diet.test.ts
@@ -2,59 +2,100 @@ import supertest from "supertest";
 import { expect } from "chai";
 import { app } from "../server/server";
 
-//const request = supertest(app);
+const request = supertest(app);
 
 describe("Diet API", () => {
+
+  let trial_diet = {
+    id: "",
+    name: "dieta de prueba",
+    category: "prueba",
+    author: "6439aa0ace6fbb3cd24a990f",
+    breakfast: "Desayuno de prueba",
+    lunch: "Almuerzo de prueba",
+    snacks: "Merienda de prueba",
+    dinner: "Cena de prueba",
+    short_description: "Descripción corta de prueba",
+    long_description: "Descripción larga de prueba",
+    picture: "imagen de prueba",
+    likes: 0,
+    comments: [
+      {
+        username: "usuario de prueba",
+        comment: "comentario de prueba"
+      }, 
+      {
+        username: "usuario de prueba 2",
+        comment: "comentario de prueba 2"
+      }
+    ]
+  };
+
+  let trial_diet2 = {
+    id: "",
+    name: "dieta de prueba 2",
+    category: "prueba",
+    author : "6439aa0ace6fbb3cd24a990f",
+  };
+
   it("Debe devolver una lista de las dietas", async () => {
     await supertest(app)
       .get("/diet")
       .expect(200)
   });
+  
 
-  /*
-   it("Debe devolver una lista de las dietas", async () => {
-    const response = await request.get("/diet");
-
-    expect(response.status).to.equal(200);
+  it ("Debe crear una nueva dieta", async () => {
+    await supertest(app)
+      .post("/diet")
+      .send(trial_diet)
+      .expect(200)
   });
 
-  it("Debe devolver una dieta específica", async () => { 
-    const response = await request.get("/diet/64554a87816a9a6f3289fa55");
-    
-    expect(response.status).to.equal(200);
+  it ("Debe devolver un error al crear una dieta, dieta no valida", async () => {
+    await supertest(app)
+      .post("/diet")
+      .send(trial_diet2)
+      .expect(500)
   });
 
-  it("Debe crear una nueva dieta", async () => {
-    const response = await request.post("/diet").send({
-        id: "",
-        name: "Baja calorica",
-        category: "Dieta para bajar de peso",
-        author: "Bruno",
-        breakfast: "Tostadas de aguacate con pimienta y sal",
-        lunch: "Pollo a la plancha con verduras al gusto y una pequeña porcion de arroz o papas guisadas",
-        snacks: "pieza de fruta o frutos secos",
-        dinner: "tortilla francesa con dos huevos",
-        short_description: "Dieta sencilla para una baja de peso progresiva y saludable",
-        long_description: "Dieta no estricta para un estilo de vida saludable y una baja de peso progresiva, recuerda siempre llevar un consumo de agua adecuado y realizar actividades fisicas de su preferencia",
-        picture: "siguiendolasdietitas.jpg",
-        likes: 69,
-        comments: []
-    });
 
-    expect(response.status).to.equal(200);
+  it("Debe devolver una dieta específica", async () => {
+    await supertest(app)
+    .get("/diet/" + trial_diet.name)
+    .expect(200)
   });
 
+  it("Debe devolver un error al buscar una dieta", async () => {
+    await supertest(app)
+    .get("/diet/prueba_fallo")
+    .expect(404)
+  });
+  
   it("Debe actualizar una dieta", async () => {
-    const response = await request.patch("/diet/64554a87816a9a6f3289fa55").send({
-        id: "1212121212",
-        name: "Esta dieta no se borra", 
-    });
-    expect(response.status).to.equal(200);
-  }); */
-});
+    await supertest(app)
+    .patch("/diet/" + trial_diet.name)
+    .send({breakfast: "Desayuno de prueba actualizado"})
+    .expect(200) 
+  });
 
-/* // TODO: comprobar esto para que se haga bien (:
-after(() => {
-  process.exit(0);
+  it("Debe devolver un error al actualizar una dieta", async () => {
+    await supertest(app)
+    .patch("/diet/prueba_fallo")
+    .send({breakfast: "Desayuno de prueba actualizado"})
+    .expect(404)
+  });
+  
+  
+  it("Debe borrar una dieta", async () => {
+    await supertest(app)
+    .delete("/diet/" + trial_diet.name)
+    .expect(200)
+  });
+
+  it("Debe devolver un error al borrar una dieta", async () => {
+    await supertest(app)
+    .delete("/diet/prueba_fallo")
+    .expect(404)
+  });
 });
- */
