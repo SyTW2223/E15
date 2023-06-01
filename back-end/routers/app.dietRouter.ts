06e558c7f96a08736ee3f4178a9fd10c26434d00
alu0101123677 <alu0101123677@ull.edu.es>
2023-06-01 19:59:14 +0100

    update[backend]: update diets router
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>
    Co-authored-by: alu0101101507 <alu0101101507@users.noreply.github.com>

--- back-end/routers/app.dietRouter.ts
+++ back-end/routers/app.dietRouter.ts
@@ -10,51 +10,60 @@ export const dietR = express.Router();
 dietR.use(bodyParser.json());
 
 dietR.post('/diet', async (req, res) =>{
-  // Author
-  console.log(req.body);
-  const author = await User.findOne({_id: req.body.author});
-
-  // Comments
-  const comments = req.body.comments;
-  const new_comments = [];
-  for (let i = 0; i < comments.length; i++) {
-    const comment = comments[i];
-    const new_comment = new Comment({username: comment.username, comment: comment.comment})
-    new_comments.push(new_comment);
-  }
-
-  const new_diet = new Diet({id: Math.floor(Math.random() * 1000000), name: req.body.name, category: req.body.category, author: author, 
-                            breakfast: req.body.breakfast, lunch: req.body.lunch, snacks: req.body.snacks, dinner: req.body.dinner, 
-                            short_description: req.body.short_description, long_description: req.body.short_description, picture: req.body.picture,
-                            likes: req.body.likes, comments: comments});
-
-  new_diet.save();
-  
-  res.status(200).send({ msg: "Dieta creada correctamente" });
+  await User.findOne({_id: req.body.author})
+  .then((author) =>{
+    if (!author) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    const comments = req.body.comments;
+    const new_comments = [];
+    for (let i = 0; i < comments.length; i++) {
+      const comment = comments[i];
+      const new_comment = new Comment({username: comment.username, comment: comment.comment})
+      new_comments.push(new_comment);
+    }
+    const new_diet = new Diet({id: Math.floor(Math.random() * 1000000), name: req.body.name, category: req.body.category, author: author,
+      breakfast: req.body.breakfast, lunch: req.body.lunch, snacks: req.body.snacks, dinner: req.body.dinner,
+      short_description: req.body.short_description, long_description: req.body.short_description, picture: req.body.picture,
+      likes: req.body.likes, comments: comments});
+    new_diet.save();
+    res.status(200).send({ msg: "Dieta creada correctamente" });
+  })
+  .catch(() => {
+    return res.status(500).json({ error: "Error interno del servidor" });
+  });
 });
 
 dietR.get('/diet', async(req, res) =>{
-  const diets = await Diet.find();
-  console.log(diets);
-  res.status(200).json(diets);
-});
-
-dietR.get('/diet/:id', async(req, res) =>{
-  try {
-    const diets = await Diet.findOne({_id: req.params.id});
+  await Diet.find()
+  .then((diets) =>{
+    if (!diets) {
+      return res.status(404).json({ error: "No hay dietas" });
+    }
     console.log(diets);
     res.status(200).json(diets);
-  } catch (error) {
-    res.status(401)
-  }
+  })
+  .catch(() => {
+    return res.status(500).json({ error: "Error interno del servidor" });
+  });
 });
 
-dietR.patch('/diet/:id', async(req, res) =>{
-  console.log(req.body);
-  const { id } = req.params
-  const diet_update = req.body;
+dietR.get('/diet/:name', async(req, res) =>{
+  await Diet.findOne({name: req.params.name})
+  .then((diet) =>{
+    if (!diet) {
+      return res.status(404).json({ error: "Dieta no encontrada" });
+    }
+    console.log(diet);
+    res.status(200).json(diet);
+  })
+  .catch(() => {
+    return res.status(500).json({ error: "Error interno del servidor" });
+  });
+});
 
-  await Diet.findByIdAndUpdate({_id: id}, diet_update)
+dietR.patch('/diet/:name', async(req, res) =>{
+  await Diet.findOneAndUpdate({name: req.params.name}, req.body)
   .then((diet) =>{
     if(!diet){
         return res.status(404).send({ msg: 'Dieta no encontrada' })
@@ -68,13 +77,12 @@ dietR.patch('/diet/:id', async(req, res) =>{
     })
 });
 
-dietR.delete('/diet/:id', async(req, res)=>{
-  console.log(req.body);
-  await Diet.findByIdAndDelete({_id: req.params.id})
+dietR.delete('/diet/:name', async(req, res)=>{
+  await Diet.findOneAndDelete({name: req.params.name})
   .then((Diet)=>{
     if(!Diet){
-    return res.status(404).send({ msg: "Dieta no encontrada" });
-  }
+      return res.status(404).send({ msg: "Dieta no encontrada" });
+    }
     return res.status(200).send({ msg: "Dieta eliminada correctamente" });
   })
   .catch(()=>{
