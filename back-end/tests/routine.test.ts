2eba2ee64565aec97655bf43d91271048a6d0384
alu0101123677 <alu0101123677@ull.edu.es>
2023-06-02 18:04:40 +0100

    test[backend]: update test routine
    Co-authored-by: Carla Cristina Olivares Rodríguez <ccolivares@users.noreply.github.com>
    Co-authored-by: alu0101101507 <alu0101101507@users.noreply.github.com>

--- back-end/tests/routine.test.ts
+++ back-end/tests/routine.test.ts
@@ -1,47 +1,92 @@
 import supertest from "supertest";
-import { expect } from "chai";
 import { app } from "../server/server";
 
-const request = supertest(app);
-
 describe("Routine API", () => {
-  it("Debe devolver una lista de rutinas", async () => {
-    const response = await request.get("/routine");
+  let trial_routine = {
+    id: "",
+    name: "rutina de prueba",
+    description: "Descripción de prueba",
+    category: "prueba",
+    author: "6439aa0ace6fbb3cd24a990f",
+    exercises: ["6442d4aa677db19baea771c1","6442d84b53af6d36441838c1","6442d88753af6d36441838c7"],
+    equipment_needed: true,
+    avg_duration: 3,
+    likes: 0,
+    comments: [
+      {
+        username: "usuario de prueba",
+        comment: "comentario de prueba"
+      },
+      {
+        username: "usuario de prueba 2",
+        comment: "comentario de prueba 2"
+      }
+    ],
+    picture: "imagen de prueba"
+  };
 
-    expect(response.status).to.equal(200);
+  let trial_routine2 = {
+    id: "",
+    name: "rutina de prueba 2",
+    category: "prueba",
+    author : "6439aa0ace6fbb3cd24a990f",
+  };
+
+  it ("Debe devolver una lista de las rutinas", async () => {
+    await supertest(app)
+      .get("/routine")
+      .expect(200)
   });
 
-  it("Debe devolver una rutina específica", async () => { 
-    const response = await request.get("/routine/64554733be32f08427b7d840");
-    
-    expect(response.status).to.equal(200);
+  it ("Debe crear una nueva rutina", async () => {
+    await supertest(app)
+      .post("/routine")
+      .send(trial_routine)
+      .expect(200)
   });
 
-  it("Debe crear un nueva rutina", async () => {
-    const response = await request.post("/routine").send({
-      name: "Megamix",
-      description: "Mega rutina",
-      category: "Piernas",
-      author: "Bruno",
-      exercises: ["6442d4aa677db19baea771c1","6442d84b53af6d36441838c1","6442d88753af6d36441838c7"],
-      equipment_needed: true,
-      avg_duration: 3,
-      reps: 4,
-      sets: 2,
-      picture: "Imagen",
-      likes: 40,
-      comments: []
-    });
-
-    expect(response.status).to.equal(200);
+  it ("Debe devolver un error al crear una rutina, rutina no valida", async () => {
+    await supertest(app)
+      .post("/routine")
+      .send(trial_routine2)
+      .expect(500)
   });
 
-  it("Debe actualizar un rutina", async () => {
-    const response = await request.patch("/routine/64554733be32f08427b7d840").send({
-        id: "1",
-    });
-    expect(response.status).to.equal(200);
+  it ("Debe devolver una rutina específica", async () => {
+    await supertest(app)
+      .get("/routine/" + trial_routine.name)
+      .expect(200)
   });
 
-  // TODO: DELETE RUTINE
+  it ("Debe devolver un error al buscar una rutina, rutina no valida", async () => {
+    await supertest(app)
+      .get("/routine/prueba_fallo")
+      .expect(404)
+  });
+
+  it ("Debe actualizar una rutina", async () => {
+    await supertest(app)
+      .patch("/routine/" + trial_routine.name)
+      .send({ description: "Descripción de prueba actualizada" })
+      .expect(200)
+  });
+
+  it ("Debe devolver un error al actualizar una rutina, rutina no valida", async () => {
+    await supertest(app)
+      .patch("/routine/prueba_fallo")
+      .send({ description: "Descripción de prueba actualizada"})
+      .expect(404)
+  });
+
+  it ("Debe eliminar una rutina", async () => {
+    await supertest(app)
+      .delete("/routine/" + trial_routine.name)
+      .expect(200)
+  });
+
+  it ("Debe devolver un error al eliminar una rutina, rutina no valida", async () => {
+    await supertest(app)
+      .delete("/routine/prueba_fallo")
+      .expect(404)
+  });
 });
